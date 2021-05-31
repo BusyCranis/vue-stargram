9a4a5d32b0ff55656aa22941fa3462d7617ffe7f
ilsung12 <a01041118225@gmail.com>
2021-05-31 15:22:59 +0900

    💄 Create UI and Post.vue

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <div :v-for="(a, i) in postData.length" :key="a"> -->
+  <div>
+    <Post :postData="postData[i]" v-for="(a, i) in postData" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post";
+
+export default {
+  name: "Container",
+  props: {
+    postData: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
